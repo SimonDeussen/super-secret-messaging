8eda0c5c80bc420dddf5769e3e86ade6caacf837
Deussen Simon <sd092@hdm-stuttgart.de>
2018-02-22 14:16:37 +0100

    milestone 1 create dummy application dummy click listener works

--- out/script/core.cljs
+++ out/script/core.cljs
@@ -13,60 +13,56 @@
 (defn emitSocket [socketName msg]
   (js-invoke js/socket "emit" socketName msg))
 
-(emitSocket "hello" "asdfadfafa")
 
-; (emitSocket "hello" "from clojurescript")
+(defn getTextContent [id]
+  (-> js/document
+    (.getElementById id)
+    (.-value)))
 
-; (defn getTextContent [id]
-;   (-> js/document
-;     (.getElementById id)
-;     (.-value)))
-;
-; (defn getElement [id]
-;   (-> js/document
-;     (.getElementById id)))
-;
-; (println (getTextContent "submit"))
-;
-;
-; (defn addClick [id handler]
-;   (.addEventListener (getElement id) "click" handler))
-;
-; (addClick "submit"
-;   (fn a []
-;   (emitSocket
-;     "hello"
-;     (getTextContent "textInput"))
-;   (println (getTextContent "textInput"))))
+(defn getElement [id]
+  (-> js/document
+    (.getElementById id)))
 
-; (comment "
-; TODO
-;
-;
-;
-; SAVE
-;
-; getMessage() DONE
-;   ret message
-;
-; encryptMessage(message)
-;   ret key
-;       cmessage
-;
-; hashMessage(cmessage)
-;   ret dbKey
-;
-; saveMessage(dbkey, cmessage)
-;
-; GET
-;
-; splitUrl()
-;   ret dkbey
-;       key
-;
-; getDataAndDelete(dkbey)
-;   ret cmessage
-;
-; decryptMessage(key, cmessage)
-;   ret message
-; ")
+
+(defn addClick [id handler]
+  (.addEventListener (getElement id) "click" handler))
+
+(defn dummyClick []
+  (emitSocket
+    "hello"
+    (getTextContent "textInput"))
+  (println (getTextContent "textInput")))
+
+(addClick "submit" dummyClick)
+
+(comment "
+TODO
+
+
+
+SAVE
+
+getMessage() DONE
+  ret message
+
+encryptMessage(message)
+  ret key
+      cmessage
+
+hashMessage(cmessage)
+  ret dbKey
+
+saveMessage(dbkey, cmessage)
+
+GET
+
+splitUrl()
+  ret dkbey
+      key
+
+getDataAndDelete(dkbey)
+  ret cmessage
+
+decryptMessage(key, cmessage)
+  ret message
+")
--- out/script/core.js
+++ out/script/core.js
@@ -9,6 +9,20 @@ cljs.core.js_invoke.call(null,socket,"emit","hello","clojure");
 script.core.emitSocket = (function script$core$emitSocket(socketName,msg){
 return cljs.core.js_invoke.call(null,socket,"emit",socketName,msg);
 });
-script.core.emitSocket.call(null,"hello","asdfadfafa");
+script.core.getTextContent = (function script$core$getTextContent(id){
+return document.getElementById(id).value;
+});
+script.core.getElement = (function script$core$getElement(id){
+return document.getElementById(id);
+});
+script.core.addClick = (function script$core$addClick(id,handler){
+return script.core.getElement.call(null,id).addEventListener("click",handler);
+});
+script.core.dummyClick = (function script$core$dummyClick(){
+script.core.emitSocket.call(null,"hello",script.core.getTextContent.call(null,"textInput"));
+
+return cljs.core.println.call(null,script.core.getTextContent.call(null,"textInput"));
+});
+script.core.addClick.call(null,"submit",script.core.dummyClick);
 
 //# sourceMappingURL=core.js.map
