a33e8a04898f47c1188e5e1c2ca2e2325e2a85ea
marius <marius.hahn@canoo.com>
2018-02-22 15:40:55 +0100

    removed useless files

--- build.clj
+++
@@ -1,7 +0,0 @@
-(require 'cljs.build.api)
-
-(cljs.build.api/build "src"
-  {:main 'script.core
-   :foreign-libs [{:file "node_modules/socket.io-client/dist"
-                   :provides "socket.io"}]
-   :output-to "out/main.js"})
