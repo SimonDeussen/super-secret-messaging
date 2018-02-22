acfe667b670cc4cbc34b077213e6525b84143d26
Deussen Simon <sd092@hdm-stuttgart.de>
2018-02-22 15:37:51 +0100

    deleted old folders

--- ssm/project.clj
+++ ssm/project.clj
@@ -1,6 +1,8 @@
 (defproject hello-browser "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2411"]]
+                 [org.clojure/clojurescript "0.0-2411"]
+                 [buddy "2.0.0"]
+                 [com.cognitect/transit-cljs "0.8.243"]]
 
   :plugins [[lein-cljsbuild "1.0.3"]]
 
